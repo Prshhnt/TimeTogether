9da2e7edc18e30abf6a863e103bea8604ba91f74
Prashant <prshhant@gmail.com>
2025-07-09 07:15:45 +0530

    Sync local project with all deletions and changes

--- src/socket.js
+++
@@ -1,8 +0,0 @@
-// src/socket.js
-import { io } from 'socket.io-client';
-
-const SOCKET_URL = 'http://192.168.240.156:4000'; // Change if deploying
-
-export const socket = io(SOCKET_URL, {
-  autoConnect: false,
-});
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+import { resolve } from 'path';
+
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '.'),
+      '@auth': resolve(__dirname, 'auth'),
+    },
+  },
   css: {
     postcss: './postcss.config.js',
   },
